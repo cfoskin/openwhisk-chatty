094e50621778e243ee8116823e7cb81eedf07c4c
Peter Braun <pbraun@redhat.com>
2018-03-14 09:18:06 +0100

    add README

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const googlehome = require('google-home-notifier');
 
 googlehome.ip('192.168.43.237', 'cz');
 
-exports.main = function () {
-  googlehome.notify('Hello from Openwhisk', function (res) {
+exports.main = function (args) {
+  var article = args.article || "Red Fedora";
+  googlehome.notify('Wow what a nice ' + article, function (res) {
     console.log(res);
   });
 
